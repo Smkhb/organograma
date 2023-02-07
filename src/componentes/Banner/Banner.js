76c5f1cc6b3ea744dc4fed1ff6077a4582577451
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-07 15:13:05 -0300

    Campo de texto

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,5 +1,5 @@
 import './Banner.css'
-function Banner(){
+export const Banner = ()=> {
     return (
         <header className='banner'>
             <img src="/imagens/banner.png" alt='O Banner principal da pagina do Organo'/>
@@ -7,4 +7,3 @@ function Banner(){
     )
 
 }
-export default Banner
