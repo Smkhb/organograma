3c3393e13f3c3c75d5f651b7896e8edc4612a7a2
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-08 17:12:35 -0300

    Refatorando código duplicado

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,9 +1,12 @@
 import "./Time.css";
 
 const Time = (props) =>{
+    const fundo = { backgroundColor: props.corSecundaria };
+
+    const border = { borderColor: props.corPrimaria };
     return(
-        <section className="time">
-            <h3>{props.nome}</h3>
+        <section className="time" style={fundo}>
+            <h3 style={border}>{props.nome}</h3>
         </section>
     )
 }
